8211838f710450fbbc294b94ef75eba00878e97b
Gan-Erdene <ganaa0830@gmail.com>
2016-12-11 14:26:37 +0800

    cdscds

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -1,136 +1,17 @@
-<header>
-    <!-- Mobile Menu Start -->
-    <div class="mobile-menu-area navbar-fixed-top hidden-sm hidden-md hidden-lg">
-        <nav class="mobile-menu" id="mobile-menu">
-            <div class="sidebar-nav">
-                <ul class="nav side-menu">
-                    <li class="sidebar-search">
-                        <div class="input-group custom-search-form">
-                            <input type="text" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                    <button class="btn mobile-menu-btn" type="button">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                        </div>
-                        <!-- /input-group -->
-                    </li>
-                    @foreach($menus as $item)
-                      <li><a href="{{$item->url}}">{{$item->title}}</a> </li>
-                      @endforeach
+ <?php $sub_menus = \TCG\Voyager\Models\MenuItem::where('menu_id', 2)->get(); ?>
+<header class="clearfix">
+  <nav id="main-menu" class="left navigation">
+    <ul class="sf-menu no-bullet inline-list m0">
+        @foreach($sub_menus as $menu)
+        <li><a href="{{$menu->url}}" @if($loop->index === 0) class="active" @endif>{{$menu->title}}</a></li>
+        @endforeach
+    </ul>
+  </nav>
 
-                </ul>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="top_header_icon">
-                <span class="top_header_icon_wrap">
-                        <a target="_blank" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
-                    </span>
-                <span class="top_header_icon_wrap">
-                        <a target="_blank" href="#" title="Facebook"><i class="fa fa-facebook"></i></a>
-                    </span>
-                <span class="top_header_icon_wrap">
-                        <a target="_blank" href="#" title="Google"><i class="fa fa-google-plus"></i></a>
-                    </span>
-                <span class="top_header_icon_wrap">
-                        <a target="_blank" href="#" title="Vimeo"><i class="fa fa-vimeo"></i></a>
-                    </span>
-                <span class="top_header_icon_wrap">
-                        <a target="_blank" href="#" title="Pintereset"><i class="fa fa-pinterest-p"></i></a>
-                    </span>
-            </div>
-            <div id="showLeft" class="nav-icon">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-    </div>
-    <!-- Mobile Menu End -->
-    <!-- top header -->
-    <div class="top_header hidden-xs">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4 col-md-3">
-                    <div class="top_header_menu_wrap">
-                        <ul class="top-header-menu">
-                            <li><a href="login&registration.htm">REGISTER</a></li>
-                            <li><a href="#">LOGIN</a></li>
-                            <li><a href="contact-style-two.htm">CONTACT</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!--breaking news-->
-                <div class="col-sm-8 col-md-7">
-                    <div class="newsticker-inner">
-                        <ul class="newsticker">
-                            <li><span class="color-1">Fashion</span><a href="#">Etiam imperdiet volutpat libero eu tristique.imperdiet volutpat libero eu tristique.</a></li>
-                            <li><span class="color-2">International</span><a href="#">Curabitur porttitor ante eget hendrerit adipiscing.</a></li>
-                            <li><span class="color-3">Health</span><a href="#">Praesent ornare nisl lorem, ut condimentum lectus gravida ut.</a></li>
-                            <li><span class="color-4">technology</span><a href="#">Nunc ultrices tortor eu massa placerat posuere.</a></li>
-                            <li><span class="color-1">Travel</span><a href="#">Etiam imperdiet volutpat libero eu tristique.imperdiet volutpat libero eu tristique.</a></li>
-                        </ul>
-                        <div class="next-prev-inner">
-                            <a href="#" id="prev-button"><i class='pe-7s-angle-left'></i></a>
-                            <a href="#" id="next-button"><i class='pe-7s-angle-right'></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-2">
-                    <div class="top_header_icon">
-                        <span class="top_header_icon_wrap">
-                                <a target="_blank" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
-                            </span>
-                        <span class="top_header_icon_wrap">
-                                <a target="_blank" href="#" title="Facebook"><i class="fa fa-facebook"></i></a>
-                            </span>
-                        <span class="top_header_icon_wrap">
-                                <a target="_blank" href="#" title="Google"><i class="fa fa-google-plus"></i></a>
-                            </span>
-                        <span class="top_header_icon_wrap">
-                                <a target="_blank" href="#" title="Vimeo"><i class="fa fa-vimeo"></i></a>
-                            </span>
-                        <span class="top_header_icon_wrap">
-                                <a target="_blank" href="#" title="Pintereset"><i class="fa fa-pinterest-p"></i></a>
-                            </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="top_banner_wrap">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-md-4 col-sm-4">
-                    <div class="header-logo">
-                        <!-- logo -->
-                        <a href="/">
-                            <img class="td-retina-data img-responsive" src="/storage/{{$logo->bannerpath}}" alt="">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-xs-8 col-md-8 col-sm-8 hidden-xs">
-                    <div class="header-banner">
-                        <a href="{{$top_banner->url}}"><img class="td-retina img-responsive" src="/storage/{{$top_banner->bannerpath}}" alt=""></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- navber -->
-    <div class="container hidden-xs">
-        <nav class="navbar">
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-
-                  @foreach($menus as $item)
-                    <li id="menu_{{$item->id}}"><a href="{{$item->url}}" class="category04">{{$item->title}}</a> </li>
-                    @endforeach
-                </ul>
-            </div>
-            <!-- navbar-collapse -->
-        </nav>
-    </div>
+  <div class="search-bar right clearfix">
+    <form action="http://www.example.com">
+      <input name="s" type="text" data-value="search" value="search">
+      <input name="submit" type="submit" value="">
+    </form>
+  </div>
 </header>
