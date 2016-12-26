89d6029885ea4bf0eb306a292fd2da4ef4865c86
Gan-Erdene <ganaa0830@gmail.com>
2016-12-26 21:27:44 +0800

    =sdcdsc

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -8,39 +8,16 @@
   			       		</div>
   					</li>
   					<li class="widget four column">
-  						<h3 class="widget-title">Flicker Gallery</h3>
-  	        			<div class="flickr-widget">
-  	        				<ul class="block-grid four-up">
-  	        						<li><a href="#"><img src="upload\thumb1.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb2.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb3.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb4.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb5.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb6.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb7.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb8.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb5.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb4.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb2.jpg" alt=""></a></li>
-  	        						<li><a href="#"><img src="upload\thumb3.jpg" alt=""></a></li>
-  	        				</ul>
-  	        			</div>
+  						<h3 class="widget-title">Сурталчилгаа байршуулах</h3>
+              <div class="textwidget">
+                {!! Voyager::setting('contact_us') !!}
+              </div>
   					</li>
   					<li class="widget four column">
-  						<h3 class="widget-title">Twitter</h3>
-  			        	<div class="twitter-widget">
-  			        		<ul>
-  			        			<li>
-  			        				<p><a href="#">about 25 days ago</a> Lorem ipsum dolor sit amet nulla malesuda odio morbi nunc odio tristique: <br><a href="#">http://bit.ly/8b0wO4</a></p>
-  			        			</li>
-  			        			<li>
-  			        				<p><a href="#">about 32 days ago</a> Malesuda orci ultricies pharetra onec accimsan curcus nec lorem aecenas: <br><a href="#">http://bit.ly/8b0wO4</a></p>
-  			        			</li>
-  			        			<li>
-  			        				<p><a href="#">about 59 days ago</a> Socis vestibulum cing molestie malesuada odio onces accussam orci lorem: <br><a href="#">http://bit.ly/8b0wO4</a></p>
-  			        			</li>
-  			        		</ul>
-  			        	</div>
+  						<h3 class="widget-title">Төлбөр төлөх дараалал</h3>
+              <div class="textwidget">
+                {!! Voyager::setting('contact_us') !!}
+              </div>
   					</li>
   				</ul>
   				<!-- End Footer widgets -->
